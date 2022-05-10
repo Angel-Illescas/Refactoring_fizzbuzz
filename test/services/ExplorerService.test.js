f9a8cec38e2a605d249c41b8216ac3f44035a4f0
Angel Illescas <angelalfonso.illescas@gmail.com>
2022-05-09 22:24:13 -0500

    archivos faltantes

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -7,3 +7,19 @@ describe("Test for static method filterbymission ", () => {
       expect(explorersInNode.length).toBe(1);
     });
   })
+
+  describe("Test for static method GetAmoung ", () => {
+    test('Case 2', () => {
+      const explorers = [{mission: "node"}];
+      const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+      expect(explorersInNode.length).toBe(1);
+    });
+  })
+
+  describe("Test for static method GetforUsername ", () => {
+    test('Case 3', () => {
+      const explorers = [{mission: "node", githubUsername: "ajolonauta1"}];
+      const explorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+      expect(explorersInNode).toStrictEqual(["ajolonauta1"]);
+    });
+  })
